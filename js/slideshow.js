041d0ceabbba84edb1c8bd63a15df85dbac6bbf8
kevinvaldes3 <kevinvaldes3@gmail.com>
2017-11-15 19:52:25 -0500

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,7 +2,7 @@ $(document).on("ready",main);
 
 function main () {
 	console.log("ESTOY EN MAIN slideshow");
-$("body").append("<div id='previewSlideshow'><div id='imagen'><img src='' width='300' height='400'/><div id='descripcion'></div><div id='compra'>Para comprar llama</div><div id='cerrar'>Cerrar</div></div></div>");	
+$("body").append("<div id='previewSlideshow'><div id='imagen'><img src='' width='300' height='400'/><div id='descripcion'></div><div href='tel:+573143991115' target='_blank id='compra'>Para comprar llama</div><div id='cerrar'>Cerrar</div></div></div>");	
 /*<a  href='tel:+573143991115' target='_blank' id='wa'  >Whatsapp</a*/
 $("#slideshow img").on("click",abrirImagen);
 console.log("ESTOY DESPUES DE abrirImagen");
@@ -17,7 +17,7 @@ function abrirImagen () {
 	$("#previewSlideshow").fadeIn();
 	$("#previewSlideshow img").attr("src","img/img1.jpg");
 	$("#previewSlideshow #descripcion").text("Prenda perfecta para los amantes de la moda, es decir, tu. $50.00");
-	$("#previewSlideshow #compra").on("click",comprar);
+	
  }
 
 function cerrarImagen () {
